2746bd34680e5b6a2efe61569e5395b982ddbd80
OscarGauss <dev@oscargauss.com>
2024-12-13 19:22:18 -0400

    adding GroupsPage

---
+++ pages/groups/GroupsPage.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+import React, { useState } from 'react';
+import { GroupFiltersHead, ListGroups, MainTabsHeader } from '../../components';
+import { RE_FETCH_INTERVAL } from '../../constants';
+import { useGroup } from '../../hooks';
+import { GroupCrypto, GroupFilters, GroupPeriod } from '../../types';
+
+export const GroupsPage = ({ address }: { address: string | null }) => {
+  const [filters, setFilters] = useState<GroupFilters>({
+    period: GroupPeriod.ALL,
+    orderBy: '+amount',
+    crypto: GroupCrypto.USDC,
+    amount: 0,
+  });
+  const { getGroups } = useGroup();
+  const { isPending, isLoading, data } = useQuery({
+    refetchInterval: RE_FETCH_INTERVAL,
+    queryKey: ['groups', filters, address],
+    queryFn: () =>
+      getGroups({
+        publicKey: address,
+        crypto: filters.crypto,
+        orderBy: filters.orderBy,
+        amount: filters.amount,
+        period: filters.period,
+      }),
+  });
+
+  const loading = isPending || isLoading; // || isFetching;
+  const groups = data?.success ? data?.contents : [];
+
+  return (
+    <>
+      <MainTabsHeader />
+      <GroupFiltersHead filters={filters} setFilters={setFilters} />
+      <ListGroups groups={groups} loading={loading} />
+    </>
+  );
+};
--- types/index.ts
+++ types/index.ts
@@ -1,2 +1,3 @@
+export * from './commons';
 export * from './group';
 export * from './requests';
